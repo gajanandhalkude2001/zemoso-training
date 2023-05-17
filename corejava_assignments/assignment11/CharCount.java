eb5a18f0b61998254d279b5883485baa316629d0
gajanandhalkude2001 <130146481+gajanandhalkude2001@users.noreply.github.com>
2023-05-17 16:46:07 +0530

    Update CharCount.java

--- corejava_assignments/assignment11/CharCount.java
+++ corejava_assignments/assignment11/CharCount.java
@@ -1,3 +1,10 @@
+Question:
+I/O
+
+Using TextFile and a Map<Character,Integer>, create a program that takes the file name as a command line argument and counts the occurrence of all the different characters. 
+    Save the results in a text file.
+Answer:
+
 package corejava_assignments.assignment11;
 
 import java.util.Map;
