cd573f0f8adf47a71ee39d3f9f0206063a5d2a4d
gajanandhalkude2001 <130146481+gajanandhalkude2001@users.noreply.github.com>
2023-05-17 16:49:38 +0530

    Update PingHost.java

--- corejava_assignments/assignment3/PingHost.java
+++ corejava_assignments/assignment3/PingHost.java
@@ -1,3 +1,9 @@
+Question:
+Write a java function that will ping any host ( given as input ) and computes the median of the time taken to ping.
+
+Use the system ping utility, do not use any deprecated methods.
+
+Ans:
 package corejava_assignments.assignment3;
 
 import java.io.BufferedReader;
