bec1278534e46ba34c6491fc231f95a950b19bb7
gajanandhalkude2001 <130146481+gajanandhalkude2001@users.noreply.github.com>
2023-05-17 16:41:59 +0530

    Update FileSearch.java

--- corejava_assignments/assignment1/FileSearch.java
+++ corejava_assignments/assignment1/FileSearch.java
@@ -1,3 +1,8 @@
+Question:
+Create a java program to search through the home directory and look for files that match a regular expression. 
+    The program should be able to take inputs repeatedly and should print out the full absolute path of the matching files found.
+
+
 package corejava_assignments.assignment1;
 
 import java.io.File;
