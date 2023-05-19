a0a6d566cda85345532aa07cf9a3ccdbfc1bc13e
gajanandhalkude2001 <130146481+gajanandhalkude2001@users.noreply.github.com>
2023-05-19 19:59:37 +0530

    Update Assignment-2.5.sql

--- DBMS/Assignment-2.5.sql
+++ DBMS/Assignment-2.5.sql
@@ -1,8 +1,7 @@
--- select count(film_id) from film_actor where film_id 
--- 	IN (select film_id from film where rating="R");
--- select film_id from film_actor where actor_id 
--- 	IN (select actor_id from actor where first_name="SUSAN" and last_name="WILSON");
+Question:
+Find out the number of R rated movies rented by “SUSAN WILSON”.
 
+Ans:
 SELECT COUNT(film_id) FROM film_actor WHERE film_id 
 	IN (SELECT film_id FROM film WHERE rating = 'R') AND actor_id 
 		IN (SELECT actor_id FROM actor WHERE first_name = 'Susan' AND last_name = 'Wilson');
